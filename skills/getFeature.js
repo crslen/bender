471e67fc986c763a598a348b88ee476f613fbe20
root <root@ip-172-31-41-193.us-west-2.compute.internal>
2018-09-07 13:18:06 +0000

    Update get feature feature

--- skills/getFeature.js
+++ skills/getFeature.js
@@ -14,48 +14,48 @@ var colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
   '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
 ];
 //enable luis ai
-var luis = require('botkit-middleware-luis');
-var luisOptions = {
-  serviceUri: process.env.serviceUri
-};
+//var luis = require('botkit-middleware-luis');
+//var luisOptions = {
+//  serviceUri: process.env.serviceUri
+//};
 
 module.exports = function(controller) {
-  controller.middleware.receive.use(luis.middleware.receive(luisOptions));
-  controller.hears(['roadmap'], 'direct_message,direct_mention,mention', function(bot, message) {
+  //controller.middleware.receive.use(luis.middleware.receive(luisOptions));
+  controller.hears(['get roadmap (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
     console.log(JSON.stringify(message));
-    if (message.topIntent.intent == "roadmap") {
-      bot.reply(message, "Please hold.....");
-      
-      var strSearch = message.entities[0].entity;
-      valFunc.getRoadmap(strSearch, function(res) {
-        if (res.length == 0) {
+    //  if (message.topIntent.intent == "roadmap") {
+    bot.reply(message, "Please hold.....");
+
+    var strSearch = message.match[1];
+    valFunc.getRoadmap(strSearch, function(res) {
+      if (res.length == 0) {
+        bot.reply(message, {
+          text: "Hmm something bad happened, I can't query this information."
+        });
+      } else {
+        var jsonParse = JSON.stringify(res);
+        console.log("return: " + jsonParse);
+        var jsonStr = JSON.parse(jsonParse);
+        for (var i = 0; i < jsonStr.length; i++) {
+          //var date = new Date(jsonStr[i].date_inserted)
+          //var otDate = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+          var rmMessage = '' +
+            '*Feature Title:* ' + jsonStr[i].feature_title + '\n' +
+            '*Category:* ' + jsonStr[i].feature_category + '\n' +
+            '*Status:* ' + jsonStr[i].feature_status + '\n' +
+            '*Owner:* ' + jsonStr[i].feature_owner + '\n' +
+            '*Website Visibility:* `' + jsonStr[i].feature_website_visibility + '`\n' +
+            '*Internal Milestone:* ' + jsonStr[i].feature_internal_milestone + '\n' +
+            '*Description:* ' + jsonStr[i].feature_description + '\n';
           bot.reply(message, {
-            text: "Hmm something bad happened, I can't query this information."
+            attachments: [{
+              color: colorArray[i],
+              text: rmMessage
+            }]
           });
-        } else {
-          var jsonParse = JSON.stringify(res);
-          console.log("return: " + jsonParse);
-          var jsonStr = JSON.parse(jsonParse);
-          for (var i = 0; i < jsonStr.length; i++) {
-            //var date = new Date(jsonStr[i].date_inserted)
-            //var otDate = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
-            var rmMessage = '' +
-              '*Feature Title:* ' + jsonStr[i].feature_title + '\n' +
-              '*Category:* ' + jsonStr[i].feature_category + '\n' +
-              '*Status:* ' + jsonStr[i].feature_status + '\n' +
-              '*Owner:* ' + jsonStr[i].feature_owner + '\n' +
-              '*Website Visibility:* `' + jsonStr[i].feature_website_visibility + '`\n' +
-              '*Internal Milestone:* ' + jsonStr[i].feature_internal_milestone + '\n' +
-              '*Description:* ' + jsonStr[i].feature_description + '\n';
-            bot.reply(message, {
-              attachments: [{
-                color: colorArray[i],
-                text: rmMessage
-              }]
-            });
-          }
         }
-      });
-    }
+      }
+    });
+    //}
   });
 }; /*the end*/
